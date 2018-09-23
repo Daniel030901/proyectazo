a1e670b20b4e5f8f4f882605c81aebb1f283050e
Daniel030901 <edgardanielacostarosales@outlook.com>
2018-09-23 15:18:04 -0500

    desde barra

--- web/index.php
+++ web/index.php
@@ -1,6 +1,8 @@
 <?php
 #$filepath para guardar prro
-echo ' <img src="barcode.php?text=123456789&size=50&orientation=horizontal&
+$m=$_GET['selecciona'];
+#echo $m;
+echo ' <img src="barcode.php?text='.$m.'&size=50&orientation=horizontal&
  codetype=Code39&print=true&sizefactor=2"/>';
  echo "</br>";
  echo '<a href="barcode.php?text=123456789&size=50&orientation=horizontal&
