6bc7ee09d2df5a14a4646d215eedf52dfc585ff1
Daniel030901 <edgardanielacostarosales@outlook.com>
2018-09-23 14:58:34 -0500

    hola

---
+++ web/index.php
@@ -0,0 +1,8 @@
+<?php
+#$filepath para guardar prro
+echo ' <img src="barcode.php?text=123456789&size=50&orientation=horizontal&
+ codetype=Code39&print=true&sizefactor=2"/>';
+ echo "</br>";
+ echo '<a href="barcode.php?text=123456789&size=50&orientation=horizontal&
+  codetype=Code39&print=true&sizefactor=2">Generar</a>';
+ ?>
